913398f70f2c6169121e124a54c07cced97adcdd
K1K3-08 <109540032+K1K3-08@users.noreply.github.com>
2025-06-12 15:24:26 -0600

    still testing

---
+++ webUI.py
@@ -0,0 +1,16 @@
+from local_file_picker import local_file_picker
+
+from nicegui import ui
+
+
+async def pick_file() -> None:
+    result = await local_file_picker('~', multiple=True)
+    ui.notify(f'You chose {result}')
+
+
+@ui.page('/')
+def index():
+    ui.button('Choose file', on_click=pick_file, icon='folder')
+
+
+ui.run()
